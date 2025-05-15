ea284d8d1dd5075391a979a2bee0623283e87177
Jarko <jarko.van.zeumeren@strandreddingsbrigade.nl>
2025-05-15 13:31:49 +0200

    changed some images for the slider for SEO

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,12 +1,12 @@
 <div class="slider-wrapper container my-5">
     <div class="glider-contain">
         <div class="glider">
-            <img src="https://perkgroen.nl/images/Artikel_Jungle_tuin_met_tropische_stijl_planten/De_jungletuin_met_ordening_in_een_meerlagen-structuur_resulteert_in_een_spannend_evenwicht_die_blijft_boeien__PerkLandscapes-com.jpg" alt="Jungletuin 1">
-            <img src="https://perkgroen.nl/images/Artikel_Jungle_tuin_met_tropische_stijl_planten/combine_different_bold_leaf_plants_in_the_understory_of_a_jungle_garden__PERKlandscapes-com.jpg" alt="Jungletuin 2">
-            <img src="https://jungletuin.nl/wp-content/uploads/2021/03/GIXZ1247_copy.jpg" alt="Jungletuin 4">
-            <img src="https://perkgroen.nl/images/Artikel_Jungle_tuin_met_tropische_stijl_planten/De_jungletuin_met_ordening_in_een_meerlagen-structuur_resulteert_in_een_spannend_evenwicht_die_blijft_boeien__PerkLandscapes-com.jpg" alt="Jungletuin 1">
-            <img src="https://perkgroen.nl/images/Artikel_Jungle_tuin_met_tropische_stijl_planten/combine_different_bold_leaf_plants_in_the_understory_of_a_jungle_garden__PERKlandscapes-com.jpg" alt="Jungletuin 2">
-            <img src="https://jungletuin.nl/wp-content/uploads/2021/03/GIXZ1247_copy.jpg" alt="Jungletuin 4">
+            <img src="{{ asset('images/slider/slider1.jpg') }}" alt="Jungletuin 1">
+            <img src="{{ asset('images/slider/slider2.jpg') }}" alt="Jungletuin 2">
+            <img src="{{ asset('images/slider/slider3.jpg') }}" alt="Jungletuin 3">
+            <img src="{{ asset('images/slider/slider1.jpg') }}" alt="Jungletuin 1">
+            <img src="{{ asset('images/slider/slider2.jpg') }}" alt="Jungletuin 2">
+            <img src="{{ asset('images/slider/slider3.jpg') }}" alt="Jungletuin 3">
         </div>
     </div>
 </div>
